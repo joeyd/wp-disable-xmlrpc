e6e2a92c5238e3ba4824a82fa3b6ecb5aa559310
JoeyD <williamdurham@gmail.com>
2014-03-27 21:46:11 -0400

    update

--- wp-disable-xmlrpc.php
+++ wp-disable-xmlrpc.php
@@ -10,6 +10,8 @@
 
 add_filter( 'xmlrpc_methods', function( $methods ) {
    unset( $methods['pingback.ping'] );
+   unset( $methods['pingback.extensions.getPingbacks'] );
    return $methods;
 });
 ?>
+
